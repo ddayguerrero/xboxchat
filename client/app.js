afb6e5f05023f0bd040a9864b74820084f625349
Darrel-Day Guerrero <ddayy.guerrero@hotmail.com>
2015-07-14 23:41:25 -0400

    Insert comments to DB

--- client/app.js
+++ client/app.js
@@ -14,3 +14,18 @@ Template.messages.helpers({
 //         }
 //     }
 // });
+
+Template.footer.events({
+  'keypress input': function(e) {
+    var inputVal = $('.input-box_text').val();
+    if(!!inputVal) {
+      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+      if (charCode == 13) {
+        e.stopPropagation();
+        Messages.insert({chattext: $('.input-box_text').val()});
+        $('.input-box_text').val("");
+        return false;
+      }    
+    }
+  }
+});
